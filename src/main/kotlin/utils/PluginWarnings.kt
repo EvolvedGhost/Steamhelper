eabf1a92ca07612776b58c008f732b7a7a66dfdc
Ghost <727511070@qq.com>
2022-05-24 02:31:32 +0800

    更新错误提示

--- src/main/kotlin/utils/PluginWarnings.kt
+++ src/main/kotlin/utils/PluginWarnings.kt
@@ -45,14 +45,12 @@ private fun exceptionTranslator(e: String): String {
         "请求被拒绝，请检查网络是否正常或代理是否正常"
     else if (e.contains("unable to find valid certification path to requested target"))
         "证书错误，请检查网络环境或者切换到请求模式1"
-    else if (e.contains("java.net.SocketTimeoutException: connect timed out"))
+    else if (e.contains("java.net.SocketTimeoutException: Connect timed out"))
         "连接已超时，请检查是否能够流畅访问相关网站"
     else if (e.contains("java.net.UnknownHostException"))
         "无法解析域名，请检查获取源是否正确"
     else if (e.contains("java.lang.IllegalArgumentException: Illegal pattern character"))
         "模板格式错误，请检查timeFormat的格式"
-    else if (e.contains("java.lang.RuntimeException: CronExpression"))
-        "Cron格式错误，请检查Config中所有Cron的格式"
     else if (e.contains("java.lang.RuntimeException: CronExpression"))
         "Cron格式错误，请检查Config中所有Cron的格式"
     else "未知错误：$e"
